8f77f2f9458545abd82c08cf40160abf44a71185
charliecronin3 <87365814+charliecronin3@users.noreply.github.com>
2022-08-02 20:23:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,12 @@ let server = app.listen(port, () => { /* Server Instance by App Instance */
 
 });
 
+const isValidEmail = (email) => { /* Used to validate email */
+
+    return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+};
+
 app.get("/", (req, res) => { /* Home Route */
 
     const cookies = req.cookies;
@@ -37,12 +43,6 @@ app.get("/register", async (req, res) => {
             "success": obj !== null, /* Returns true if successful */
             "data": (obj !== null ? obj : response) /* If Object is null, return response string */
         });
-
-    };
-
-    const isValidEmail = (email) => { /* Used to validate email */
-
-        return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
     };
 
@@ -102,7 +102,7 @@ app.get("/register", async (req, res) => {
     users.set(uuid, { /* Store in Enmap DB */
 
         uuid: uuid,
-        hash: hash,
+        hash: String(hash),
         
         email: String(data["email"]).toLowerCase(),
 
@@ -117,8 +117,59 @@ app.get("/register", async (req, res) => {
 
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", async (req, res) => {
 
-    //
+    const data = req.query; /* Store Request URL Params to Variable */
+    
+    const rtn = (response, obj = null) => { /* Method used for returning Response to Client after handling Request */
+        
+        return res.json({
+            "success": obj !== null, /* Returns true if successful */
+            "data": (obj !== null ? obj : response) /* If Object is null, return response string */
+        });
+
+    };
+
+    if(data["email"] === undefined || data["pass"] === undefined) /* Validate params were specified during request */
+        return rtn("Invalid Parameters were specified!"); /* Return the error */
+    
+    /* Validate Email */
+    
+    {
+
+        const str = String((data["email"]));
+        
+        if(str.length <= 0 || str.length > 80) {
+            
+            return rtn((str.length <= 0 ? "You did not specify an Email!" : "Specified Email exceeds 80 characters!"));
+    
+        };
+
+        if(!isValidEmail(str))
+            return rtn("Specified Email is not a valid Email!");
+        
+        found = null;
+        users.forEach((userObj, uuid) => {
+
+            if(userObj.email == str.toLowerCase()) {
+
+                found = userObj;
+                return;
+
+            };
+
+        });
+
+        if(found == null)
+            return rtn("Account with that Email was not found!");
+        
+        const isValid = await bcrypt.compare(String(data["pass"]), found.hash);
+        
+        if(!isValid)
+            return rtn("Incorrect Password was specified!")
+        
+        return rtn("", users.get(found.uuid)); /* Get the RAW (unmodified) User Object from DB, do not send this to other Client Users */
+
+    };
 
 });
